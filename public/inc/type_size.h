4655596973e58d51a0b38d20f3f9d00f3123c752
nkym <2738684393@qq.com>
2023-11-19 17:05:30 +0800

    新增日志、公共模块、主模块文件目录

---
+++ public/inc/type_size.h
@@ -0,0 +1,8 @@
+#ifndef TYPE_SIZE_H
+#define TYPE_SIZE_H
+
+#define INT32 int
+#define BOOL bool
+#define VOID void
+
+#endif  // TYPE_SIZE_H
---
+++ test/src/test.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "short:" << sizeof(short) << std::endl;
+    std::cout << "int:" << sizeof(int) << std::endl;
+    std::cout << "long:" << sizeof(long) << std::endl;
+    std::cout << "long long:" << sizeof(long long) << std::endl;
+    std::cout << "unsigned short:" << sizeof(unsigned short) << std::endl;
+    std::cout << "unsigned:" << sizeof(unsigned) << std::endl;
+    std::cout << "unsigned int:" << sizeof(unsigned int) << std::endl;
+    std::cout << "unsigned long:" << sizeof(unsigned long) << std::endl;
+    std::cout << "unsigned long long:" << sizeof(unsigned long long) << std::endl;
+    std::cout << "float:" << sizeof(float) << std::endl;
+    std::cout << "double:" << sizeof(double) << std::endl;
+    std::cout << "unsigned:" << sizeof(unsigned) << std::endl;
+    std::cout << "unsigned:" << sizeof(unsigned) << std::endl;
+    std::cout << "unsigned:" << sizeof(unsigned) << std::endl;
+    std::cout << "unsigned:" << sizeof(unsigned) << std::endl;
+    std::cout << "unsigned:" << sizeof(unsigned) << std::endl;
+
+    return 0;
+}
