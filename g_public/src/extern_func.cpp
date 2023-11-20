b49513c5a013ca8e9aa338a399fadd30924f85ed
nkym <2738684393@qq.com>
2023-11-20 23:18:47 +0800

    新增公共函数，对基本数据类型进行运算

--- g_public/src/extern_func.cpp
+++ g_public/src/extern_func.cpp
@@ -1,9 +1,13 @@
 /*********************************************
-* 常用公共函数
+* 常用公共函数，该文件公共函数只用了int类型
+* 后续需要优化，适配各种基本数据类型
 * 作者：余天友
 * 创建日期：2023-11-20
 *********************************************/
 
+/*
+* 判断一个有符号数是整数、非正数、负数、非负数
+*/
 extern bool Positive(int positive)
 {
     return positive > 0;
@@ -22,4 +26,31 @@ extern bool Negative(int negative)
 extern bool None_Negative(int none_Negative)
 {
     return none_Negative >= 0;
+}
+
+/*
+* 将一个有符号数进行左移、右移操作
+*/
+int Signed_Left_Shift(int number, int position)
+{
+    return number << position;
+}
+
+int Signed_Right_Shift(int number, int position)
+{
+    return number >> position;
+}
+
+/*
+* 将一个无符号数进行左移、右移操作
+*/
+
+int Unsigned_Left_Shift(unsigned number, int position)
+{
+    return number << position;
+}
+
+int Unsigned_Right_Shift(unsigned number, int position)
+{
+    return number >> position;
 }
